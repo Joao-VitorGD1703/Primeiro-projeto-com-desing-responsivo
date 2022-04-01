3443bcbb36e85c1170bac35fff4aa7c27c4b95a7
João Vítor G Douverny <douvernyjoaovitor@gmail.com>
2022-04-01 18:44:58 -0300

    projeto finalizado

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 let countEl = document.getElementById('count-el');
+let saveEl = document.getElementById('save-el');
 
 let count = 0
 
 function increment() {
-    count = count + 1
+    count += 1
     countEl.innerText = count
     console.log(count)
 }
@@ -14,11 +15,17 @@ function decrement() {
     console.log(count)
 }
 
-function save(){
-    cont = count
+function save() {
+    let contStr = count 
+    // saveEl.innerText += contStr
+    saveEl.textContent +=  ` ${contStr} -`
+    countEl.textContent = 0
+    count = 0
+
     console.log(count)
-    
+
+
+
+    //  saveEl.innerText =+ `Previus entries: ${count} - `;
+
 }
-
-
-
