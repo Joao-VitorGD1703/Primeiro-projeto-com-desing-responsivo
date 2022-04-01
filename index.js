c45666d31b57d86f9e1217f814684573a360722e
João Vítor G Douverny <douvernyjoaovitor@gmail.com>
2022-04-01 15:22:40 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+let countEl = document.getElementById('count-el');
+
+let count = 0
+
+function increment() {
+    count = count + 1
+    countEl.innerText = count
+    console.log(count)
+}
+
+function decrement() {
+    count = count - 1
+    countEl.innerText = count
+    console.log(count)
+}
+
+function save(){
+    cont = count
+    console.log(count)
+    
+}
+
+
+
